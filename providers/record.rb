2b6851589f23c5f280737255409e15181eb344a3
Cameron Johnston <cameron@needle.com>
2012-08-15 16:09:48 -0600

    making zone_id work for domain names as well as zone identifiers

--- providers/record.rb
+++ providers/record.rb
@@ -18,11 +18,28 @@ action :create do
     @ttl ||= new_resource.ttl
   end
 
+  def zone_id
+    unless new_resource.zone_id.match(/^[A-Z0-9]{14}$/) # should match zone identifiers
+      if new_resource.zone_id.match(/\.$/) # lets add a trailing . to domains which lack them
+        zone_name = new_resource.zone_id
+      else
+        zone_name = new_resource.zone_id + "."
+      end
+      @id ||= @zones.reject!{|z| z.domain != zone_name }.first.id
+    else
+      new_resource.zone_id
+    end
+  end
+
   def zone
-    @zone ||= Fog::DNS.new({ :provider => "aws",
+    zones.get(zone_id)
+  end
+
+  def zones
+    @zones ||= Fog::DNS.new({ :provider => "aws",
                              :aws_access_key_id => new_resource.aws_access_key_id,
                              :aws_secret_access_key => new_resource.aws_secret_access_key }
-                           ).zones.get( new_resource.zone_id )
+                           ).zones
   end
 
   def create
